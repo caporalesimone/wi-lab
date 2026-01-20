1a1562b4dc32c84dea6077da9e270ad3fc1652d5
Furio Piccinini <furio.piccinini@external.cnh.com>
2026-01-20 17:00:37 +0100

    fixed environment.prod.ts usage

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://10.123.115.79:8080/api/v1',
+  apiUrl: 'http://10.123.115.84:8080/api/v1', // Full URL for local development (ng serve)
   authToken: 'secret-token-12345', // Change this to match your config.yaml
   pollingInterval: 5000 // 5 seconds
 };
